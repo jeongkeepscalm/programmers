18b36a9edf28bcb6ee7711eb5b3c4b8b098cb7fc
JEONGGIL\ojg01 <jeongkeepscalm@gmail.com>
2024-10-03 22:28:09 +0900

    .

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import lv2._001;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String min_and_max = _001.solution("-4 -3 2 4");
+        System.out.println("001. min_and_max = " + min_and_max);
+
+    }
+}
---
+++ src/lv2/_001.java
@@ -0,0 +1,35 @@
+package lv2;
+
+import java.util.Arrays;
+
+/*
+    최댓값과 최솟값
+    문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다.
+    str 에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution 을 완성하세요.
+    예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
+
+    public String solution(String s) {
+        String[] arr = s.split(" ");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (String str : arr) {
+            int num = Integer.parseInt(str);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        return min + " " + max;
+    }
+
+ */
+public class _001 {
+
+    public static String solution(String str) {
+        int[] numbers = Arrays.stream(str.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int max = Arrays.stream(numbers).max().getAsInt();
+        int min = Arrays.stream(numbers).min().getAsInt();
+        return min + " " + max;
+    }
+
+}
