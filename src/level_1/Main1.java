5cbd95995373526a54b0e639d2f31ca9e7b730e4
JeongGil <jeongkeepscalm@gmail.com>
2024-10-15 18:02:10 +0900

    015

--- src/level_1/Main1.java
+++ src/level_1/Main1.java
@@ -9,6 +9,12 @@ public class Main1 {
         _009.solution("pPoooyY");               // stream
         _010.theBestSolution(118372);           // StringBuilder
 
+        _012.theBestSolution(13);               // char - 0
+        _013.theOtherSolution(5, 3);         // for (삼항 연산자)
+        _014.theBestSolution(new String[]{"Jane", "Kim"});
+        _015.solution(626331);              // 삼항 연산자
+
+
 
     }
 }
