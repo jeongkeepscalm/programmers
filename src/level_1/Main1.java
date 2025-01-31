0cba5f82bd996e4556faee52df1cf6cd5b24bdce
JEONGGIL\ojg01 <jeongkeepscalm@gmail.com>
2025-01-31 15:25:25 +0900

    main edited

--- src/level_1/Main1.java
+++ src/level_1/Main1.java
@@ -10,23 +10,30 @@ import level_1._61_to_70.*;
 
 public class Main1 {
     public static void main(String[] args) {
+
         _003.theOtherSolution(4, 3); // stream
         _005.theBestSolution(123); // while
         _006.solution(5); // stream
         _008.solution(12345); // StringBuilder
         _009.solution("pPoooyY"); // stream
         _010.theBestSolution(118372); // StringBuilder
+
+
         _012.theBestSolution(13); // char - 0
         _013.theOtherSolution(5, 3); // for (삼항 연산자)
         _014.theBestSolution(new String[]{"Jane", "Kim"});
         _015.solution(626331); // 삼항 연산자
         _019.theBestSolution(new int[]{4, 3, 2, 1}); // stream
         _020.solution("00000001234");
+
+
         _021.theBestSolution(new int[]{-1, 0, 1}, new int[]{1, 0, -1}); // stream
         _023.theBestSolution("qwer");
         _025.theOtherSolution("Zbcdefg"); // basic
         _027.theBestSolution("0a123"); // exception
         _028.theBestSolution(new int[][]{{1, 2}, {2, 3}}, new int[][]{{3, 4}, {5, 6}}); // array basic
+
+
         _032.theBestSolution(new int[]{2, 2, 3, 3}, 10);
         _034.solution("3141592", "271");
         _035.theBestSolution("try hello world");
@@ -34,6 +41,8 @@ public class Main1 {
         _038.solution("a B z", 4); // stream
         _038.theBestSolution("a B z", 4); // char
         _039.theBestSolution("banana"); // map
+
+
         _041.solution(new int[]{2, 1, 3, 4, 1}); // stream
         _042.solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}); // System.arraycopy() || Arrays.copyOfRange()
         _044.theOtherSolution(new String[]{"dfs", "abc", "axx"}, 0); // ★ comparator
@@ -41,6 +50,8 @@ public class Main1 {
         _046.theBestSolution(6, new int[]{46, 33, 33, 22, 311, 50}, new int[]{27, 56, 19, 14, 14, 10}); // Binary
         _047.theBestSolution(4, new int[]{0, 300, 40, 300, 20, 70, 150, 50, 500, 1000});
         _049.solution(new String[]{"i", "drink", "water"}, new String[]{"want", "to"}, new String[]{"i", "want", "to", "drink", "water"}); // Deque        _050.theBestSolution(new int[]{1, 2, 3, 2});            // stream
+
+
         _051.theOtherSolution(5, 24); // 요일 구하기
         _052.theBestSolution(10, 3, 2); // 약수의 갯수 구하기
         _053.solution(new int[]{1, 2, 3, 4, 5});
@@ -50,6 +61,8 @@ public class Main1 {
         _057.theOtherSolution2(4, 1, new int[]{1, 2, 3, 4});
         _058.solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3});
         _060.theBestSolution(new int[]{50, 50}, new int[]{100, 241});
+
+
         _061.solution("1D2S3T*"); // pattern
     }
 
